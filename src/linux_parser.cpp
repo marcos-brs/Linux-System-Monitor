7a39a28ebba9bef86ca9c0692adba1964e77267b
Marcos Santana <brmarcossantanabr@gmail.com>
2020-06-03 22:48:29 -0300

    TODO => LinuxParser::MemoryUtilization function

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -68,7 +68,26 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization() {
+  string line, key;
+  float mem_total = 0, mem_free = 0, mem_info = 0;
+  std::ifstream filestream(kProcDirectory + kMeminfoFilename);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> key >> mem_info) {
+        if (key == "MemTotal:") {
+          mem_total = mem_info;
+        } else if (key == "MemFree:") {
+          mem_free = mem_info;
+        }
+
+        if (mem_total != 0 && mem_free != 0) break;
+      }
+    }
+  }
+  return (mem_total - mem_free) / mem_total;
+}
 
 // TODO: Read and return the system uptime
 long LinuxParser::UpTime() { return 0; }
