a4d183e6a94dc7bd2bc9ed4f487d5b3a74841ffa
Marcos Santana <brmarcossantanabr@gmail.com>
2020-06-04 04:05:45 -0300

    Fix Processor operator overloading

--- src/process.cpp
+++ src/process.cpp
@@ -52,7 +52,7 @@ long int Process::UpTime() { return LinuxParser::UpTime(pid_); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a [[maybe_unused]]) const {
+bool Process::operator<(Process const& a) const {
   long ram = std::stol(LinuxParser::Ram(pid_));
   long ram_a = std::stol(LinuxParser::Ram(a.pid_));
 
