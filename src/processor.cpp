dbbfec0a070ef82fbc0a1231835cf0efae2600e1
Marcos Santana <brmarcossantanabr@gmail.com>
2020-06-04 02:03:58 -0300

    TODO => Processor::Utilization function

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,23 @@
 #include "processor.h"
 
+#include <unistd.h>
+
+#include "linux_parser.h"
+
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  float delta_total, delta_active;
+
+  long total_jiffies_start = LinuxParser::Jiffies();
+  long active_jiffies_start = LinuxParser::ActiveJiffies();
+
+  usleep(100000);
+
+  long total_jiffies_end = LinuxParser::Jiffies();
+  long active_jiffies_end = LinuxParser::ActiveJiffies();
+
+  delta_total = total_jiffies_end - total_jiffies_start;
+  delta_active = active_jiffies_end - active_jiffies_start;
+
+  return delta_active / delta_total;
+}
