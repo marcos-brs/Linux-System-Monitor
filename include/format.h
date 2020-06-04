80c8433f486e080a16cc61805893c9de5716858b
Marcos Santana <brmarcossantanabr@gmail.com>
2020-06-03 22:30:46 -0300

    TODO => Format::ElapsedTime function

--- include/format.h
+++ include/format.h
@@ -3,6 +3,9 @@
 
 #include <string>
 
+#define SECONDS_PER_MINUTE 60
+#define SECONDS_PER_HOUR 3600
+
 namespace Format {
 std::string ElapsedTime(long times);  // TODO: See src/format.cpp
 };                                    // namespace Format
--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,8 @@
-#include <string>
-
 #include "format.h"
+
+#include <iomanip>
+#include <sstream>
+#include <string>
 
 using std::string;
 
@@ -8,4 +10,16 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  long seconds_elapsed = seconds % SECONDS_PER_MINUTE;
+  long minutes_elapsed = (seconds / SECONDS_PER_MINUTE) % SECONDS_PER_MINUTE;
+  long hours_elapsed = seconds / SECONDS_PER_HOUR;
+
+  std::stringstream formated_string;
+
+  formated_string << std::setw(2) << std::setfill('0') << hours_elapsed << ":"
+                  << std::setw(2) << std::setfill('0') << minutes_elapsed << ":"
+                  << std::setw(2) << std::setfill('0') << seconds_elapsed;
+
+  return formated_string.str();
+}
